c1a3ea19b14680f5686b01c46219b893693c292b
richjambor <richjambor@gmail.com>
2014-05-03 16:27:15 -0400

    add new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,4 +10,14 @@ class ListsController < ApplicationController
 	def new
 		@list = Wishlist.new
 	end
+	def create
+		@list = Wishlist.create wishlist_params
+		redirect_to list_path(@list)
+	end
+	
+	private
+	
+	def wishlist_params
+		params.require(:wishlist).permit :name
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- resources :lists, only: [:index, :show, :new]
+ resources :lists, only: [:index, :show, :new, :create]
  root to: 'lists#index'
 
 end
